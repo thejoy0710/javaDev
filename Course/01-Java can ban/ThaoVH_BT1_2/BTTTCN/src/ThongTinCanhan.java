364020448884b51705691bfefc36ff511b413154
hieuthao111 <hieuthao9111@gmail.com>
2015-12-19 14:44:25 +0700

    nè
    comit bài

---
+++ Course/01-Java can ban/ThaoVH_BT1_2/BTTTCN/src/ThongTinCanhan.java
@@ -0,0 +1,18 @@
+
+public class ThongTinCanhan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào mọi người lớp học java");
+		System.out.println("Họ và tên: Vàng Hiếu Thảo");
+		System.out.println("Ngày sinh: 20/11/1991");
+		System.out.println("Tình trạng hôn nhân: độc thân");
+		System.out.println("Trình độ học vấn: đại học");
+		System.out.println("Địa chỉ: 82 Thống Nhất-đỨc Trọng-Lâm đồng");
+		System.out.println("Nghề nghiệp/ Chuyên môn: IT/Network security");
+		System.out.println("Số DT: 01665896453");
+		
+		
+	}
+
+}
